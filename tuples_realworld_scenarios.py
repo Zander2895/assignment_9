a344722244d46453b1511e5f678a628b61fdf02e
Zander2895 <jacquesalexander71@gmail.com>
2024-07-15 20:14:24 -0400

    new commit

--- tuples_realworld_scenarios.py
+++ tuples_realworld_scenarios.py
@@ -25,3 +25,9 @@ class LibrarySystem:
     def display_books(self):
         for book in self.books:
             print(f"Title: {book[0]}, Author: {book[1]}, ISBN: {book[2]}")
+
+library = LibrarySystem()
+library.add_book("1984", "George Orwell", "04844881")
+library.add_book("1984", "George Orwell", "04844881")
+library.add_book("Brave New World", "Aldous Huxley", "04844662")
+library.display_books()
